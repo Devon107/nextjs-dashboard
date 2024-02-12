b8939db67197e00d32f8fa552ce8507a2cc9692a
Devon107 <abiclank@gmail.com>
2024-02-12 09:47:53 -0600

    customers table view added

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,11 +1,24 @@
 import { Metadata } from 'next';
+import Table from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from "@/app/lib/data"
 
 export const metadata : Metadata = {
     title: 'Customers',
 }
 
-export default function page() {
+export default async function page({
+    searchParams,
+}: {
+    searchParams?: {
+    query?: string;
+    page?: string;
+    };
+}) {
+    const query = searchParams?.query || '';
+
+    const customers = await fetchFilteredCustomers(query);
+
     return (
-        <p>Customers</p>
+        <Table customers={customers}/>
     )
 }
